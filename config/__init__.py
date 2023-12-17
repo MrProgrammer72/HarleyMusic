f3b14fefabe2576c1ae843110e33808c43e02aad
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-17 13:47:59 +0530

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021-present by TeamHarley@Github, < https://github.com/TeamHarley >.
+#
+# This file is part of < https://github.com/TeamHarley/HarleyMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamHarley/HarleyMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from .config import *
